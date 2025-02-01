9ff01075e8b42e8a2e90209166e17c721e11c0b8
inging-bo <thervv@kakao.com>
2025-02-01 16:31:39 +0900

    design : 기본 ui 수정
    헤더 영역
    라우터 영역 구분

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,12 @@
+import {Link} from "react-router-dom";
+
 export default function Header() {
-  return (
-      <div>
-        Header
-      </div>
-  )
+    return (
+        <div className="flex justify-between max-w-screen-lg">
+            <Link to="/">밋?잇!</Link>
+
+            {/*  로그인 or 마이 페이지  */}
+            <Link to="/account">로그인</Link>
+        </div>
+    )
 }
--- src/views/Main.jsx
+++ src/views/Main.jsx
@@ -1,5 +1,5 @@
 export default function Main() {
   return (
-      <div className="w-full h-full">main</div>
+      <div>main</div>
   )
 }
