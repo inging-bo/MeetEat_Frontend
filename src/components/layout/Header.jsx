2db59ace78d086b85701278bcb7bc2de0db04790
inging-bo <thervv@kakao.com>
2025-02-02 13:18:46 +0900

    design : header 위치 조정

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,11 +2,17 @@ import {Link} from "react-router-dom";
 
 export default function Header() {
     return (
-        <div className="flex justify-between max-w-screen-lg">
-            <Link to="/">밋?잇!</Link>
+        <header className="w-full flex justify-center h-16">
+            <div className="flex w-full justify-between max-w-screen-lg">
+                <div>
+                    <Link to="/" className="h-full px-4 flex items-center">밋?잇!</Link>
+                </div>
 
-            {/*  로그인 or 마이 페이지  */}
-            <Link to="/account">로그인</Link>
-        </div>
+                {/*  로그인 or 마이 페이지  */}
+                <div>
+                    <Link to="/account" className="h-full px-4 flex items-center">로그인</Link>
+                </div>
+            </div>
+        </header>
     )
 }
