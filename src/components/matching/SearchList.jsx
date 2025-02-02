8b0fb9f25c8aa607dd5ebfe973176a2fef77bb4b
ParkMi-Hyun <parkmihyunn@gmail.com>
2025-02-02 16:11:04 +0900

    Design: 검색된 리스트뷰 추가

---
+++ src/components/matching/SearchList.jsx
@@ -0,0 +1,13 @@
+export default function SearchList({ marker }) {
+  console.log(marker);
+  return (
+    <a href={marker.place_url} target="_blank">
+      <div className="text-left pt-2 px-4">
+        <p className="font-bold drop-shadow-md">{marker.place_name}</p>
+        <p className="text-sm drop-shadow-md">{marker.category_name}</p>
+        <p className="text-sm pb-2 drop-shadow-md">{marker.phone}</p>
+        <hr className="drop-shadow-md" />
+      </div>
+    </a>
+  );
+}
