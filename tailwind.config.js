bbeaad1a4fad190167f2f6116330178dfe4c824f
ParkMi-Hyun <parkmihyunn@gmail.com>
2025-02-02 20:15:57 +0900

    Design: 변경 메인UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      primary: "Noto Sans KR",
+    },
   },
   plugins: [require("tailwind-scrollbar-hide")],
-}
-
+};
