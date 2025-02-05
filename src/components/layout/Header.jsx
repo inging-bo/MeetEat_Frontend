901c40bc8cd1e43e2fa2b126713fef3ffc04f52f
HwangInBo <thervv@kakao.com>
2025-02-06 07:49:57 +0900

    design: 추가 헤더 로그인 회원가입 UI
    피그마 디자인 보고 수정했습니다

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,12 @@
 import {Link} from "react-router-dom";
+import HeaderLogo from "../../assets/header-logo.svg?react";
 
 export default function Header() {
     return (
-        <header className="w-full flex justify-center h-16">
-            <div className="flex w-full justify-between max-w-screen-lg">
+        <header className="fixed top-0 shadow-lg w-full flex justify-center h-16">
+            <div className="flex w-full justify-between max-w-screen-xl">
                 <div>
-                    <Link to="/" className="h-full px-4 flex items-center">밋?잇!</Link>
+                    <Link to="/" className="h-full px-4 flex items-center"><HeaderLogo/></Link>
                 </div>
 
                 {/*  로그인 or 마이 페이지  */}
--- src/views/Account.jsx
+++ src/views/Account.jsx
@@ -1,8 +1,10 @@
 import Login from "../components/account/Login.jsx";
+import Header from "../components/layout/Header.jsx";
 
 export default function Account() {
     return (
         <>
+            <Header/>
             <Login/>
         </>
     );
