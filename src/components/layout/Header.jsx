baa9dec51be1f1ec620898d6b23e937638e1ef95
inging-bo <thervv@kakao.com>
2025-02-08 15:51:32 +0900

    Feat: 추가 헤더 로그인 상태 표시
    1. 로그인 시 생성된 토큰을 저장한 로컬 스토리지를 확인합니다.
    2. 토큰이 존재하면 헤더에 로그아웃 표시를 하도록 했습니다.
    3. 로그아웃 시 서버에 빈 토큰 값을 보내고 응답 성공시 로컬스토리지에 저장된 토큰값이 삭제합니다.
    4 삭제되면 헤더에는 다시 로그인으로 표시가 됩니다.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,19 +1,58 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import HeaderLogo from "../../assets/header-logo.svg?react";
+import { useState, useEffect } from "react";
+import TwoBtnModal from "../common/TwoBtnModal.jsx";
 
 export default function Header() {
-    return (
-        <header className="fixed top-0 shadow-lg w-full flex justify-center h-16">
-            <div className="flex w-full justify-between max-w-screen-xl">
-                <div>
-                    <Link to="/" className="h-full px-4 flex items-center"><HeaderLogo/></Link>
-                </div>
 
-                {/*  로그인 or 마이 페이지  */}
-                <div>
-                    <Link to="/account" className="h-full px-4 flex items-center">로그인</Link>
-                </div>
-            </div>
-        </header>
-    )
+    // ✅ 회원가입 버튼 클릭 시 동작
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalType, setModalType] = useState(null);
+
+    const openModal = (e) => {
+        e.preventDefault();
+        setModalType("logOut");
+        setIsModalOpen(!isModalOpen)
+    }
+
+    // ✅ 모달 닫기 함수
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setModalType(null);
+    };
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // 컴포넌트가 처음 렌더링될 때 localStorage에서 accessToken 확인
+    useEffect(() => {
+        const token = localStorage.getItem("accessToken");
+        setIsLoggedIn(!!token); // accessToken이 있으면 true, 없으면 false
+    }, []);
+    return (
+      <header className="fixed top-0 shadow-lg w-full z-50 flex justify-center h-16">
+          <div className="flex w-full justify-between max-w-screen-xl">
+              <div>
+                  <Link to="/" className="h-full px-4 flex items-center">
+                      <HeaderLogo />
+                  </Link>
+              </div>
+
+              {/* 로그인 or 마이 페이지 */}
+              <div>
+                  {isLoggedIn ? (
+                    <button onClick={ (e) => openModal(e)} className="h-full px-4 flex items-center cursor-pointer">
+                        로그아웃
+                    </button>
+                  ) : (
+                    <Link to="/account" className="h-full px-4 flex items-center">
+                        로그인
+                    </Link>
+                  )}
+              </div>
+          </div>
+          {/* OneBtnModal 표시*/}
+          {isModalOpen && <TwoBtnModal type={modalType} onClose={closeModal}/>}
+      </header>
+    );
 }
