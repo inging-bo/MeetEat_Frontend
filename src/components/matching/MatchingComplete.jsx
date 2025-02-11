e330dc46214d7b76535d34677ccaab89455a23de
ParkMi-Hyun <parkmihyunn@gmail.com>
2025-02-11 23:16:15 +0900

    Feat: 추가 매칭완료 setTimeout 함수

--- src/components/matching/MatchingComplete.jsx
+++ src/components/matching/MatchingComplete.jsx
@@ -41,9 +41,8 @@ export default function MatchingComplete() {
     const toNow = now.getTime(); // 오늘까지 지난 시간(밀리 초)
     const toFirst = firstDay.getTime(); // 첫날까지 지난 시간(밀리 초)
     const passedTimeMin = (Number(toNow) - Number(toFirst)) / 60000; // 첫날부터 오늘까지 지난 시간(밀리 초)
-    console.log(passedTimeMin + "min");
-    // 매칭 완료된 이후 60분 경과 후에는 리뷰페이지로 이동
-    if (passedTimeMin >= 60) {
+
+    const goReviewPage = () => {
       const restsId = JSON.parse(window.sessionStorage.getItem("matchedData"))
         .data.matching.restaurant.id;
       const restsName = JSON.parse(window.sessionStorage.getItem("matchedData"))
@@ -57,7 +56,18 @@ export default function MatchingComplete() {
           matchedId: `${matchedId}`,
         },
       });
+    };
+
+    // 매칭 완료된 이후 60분 경과 후에는 리뷰페이지로 이동
+    if (passedTimeMin >= 60) {
+      clearTimeout(timer);
+      goReviewPage();
     }
+
+    // 매칭 완료된 이후 60분 경과 후에는 리뷰페이지로 이동
+    let timer = setTimeout(() => {
+      goReviewPage;
+    }, [360000]);
 
     // 저장된 매칭데이터 저장
     const jsonCurData = JSON.parse(
