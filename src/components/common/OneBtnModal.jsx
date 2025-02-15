f64bb3a0779c11b165378cca362808b52ae6f2d2
inging-bo <thervv@kakao.com>
2025-02-15 16:51:41 +0900

    Design: 추가 버튼 반응
    1. framer-motion 사용해 버튼 반응을 구현했습니다.
    
    2. 기본 색상 루트 추가 및 수정했습니다.
    3. 프로필 수정 버튼 클릭 후 변경 안하고 완료하면 오류 나던거 수정했습니다.

--- src/components/common/OneBtnModal.jsx
+++ src/components/common/OneBtnModal.jsx
@@ -1,14 +1,18 @@
-export default function OneBtnModal({ message, onConfirm  }) {
+import { motion } from "framer-motion"
+
+export default function OneBtnModal({ message, onConfirm }) {
   return (
     <>
       <div>
         {message}
       </div>
-      <button className="flex flex-1 px-5 py-2 bg-primary text-white rounded-md justify-center items-center cursor-pointer"
+      <motion.button
+        whileTap={{ scale: 0.95 }}
+        className="flex flex-1 px-5 py-2 bg-primary text-white rounded-md justify-center items-center cursor-pointer"
         onClick={() => onConfirm()}
       >
         확인
-      </button>
+      </motion.button>
     </>
   )
 }
