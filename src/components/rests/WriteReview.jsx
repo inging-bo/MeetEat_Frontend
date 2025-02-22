dbb85181082e8656a420960a785755eb8aeef1bb
ParkMi-Hyun <parkmihyunn@gmail.com>
2025-02-23 00:20:11 +0900

    Feat: 추가 마이페이지 아이콘

--- src/components/rests/WriteReview.jsx
+++ src/components/rests/WriteReview.jsx
@@ -108,7 +108,9 @@ export default function WriteReview() {
     formData.append("rating", starScore);
     formData.append("description", textareaValue);
     formData.append("files", postImageList);
-    console.log(formData);
+    for (const x of formData.entries()) {
+      console.log(x);
+    }
     await axios
       .post(`${import.meta.env.VITE_BE_API_URL}/restaurants/review`, formData, {
         headers: {
