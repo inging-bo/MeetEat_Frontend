75fc7b9f64bfe0d9b1b7a9b7842ec5ec3fdcc1e6
ParkMi-Hyun <parkmihyunn@gmail.com>
2025-02-26 21:48:19 +0900

    Feat: 추가 배포설정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import svgr from "vite-plugin-svgr";
 import commonjs from "vite-plugin-commonjs";
 export default defineConfig({
   plugins: [react(), commonjs(), svgr()],
+  build: {
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 });
